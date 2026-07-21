8de8acc43f9ece629842fea6f0ac4f560762bc67
ibeepboop <michellemeyer660@gmail.com>
2026-07-21 16:28:58 -0500

    lesson: update a line in a file on a new branch, predict what you see when you switch back to main

--- scratch/02-lists.js
+++ scratch/02-lists.js
@@ -19,3 +19,5 @@ console.log(days.length)
 const goodDaysGoneBad = days.map( d => `${d.date}: high of ${d.tempMax} degrees, ${d.rain}% chance of rain.` ).filter( d => d.rain < 20 )
 // so this returns an empty array. Why? For each of the days, I map each of the data to the appropriate template literal in the sentance I want to return. Ohhhhh okay. When I filter, I'm looking for d.rain, but now I just have an array of template literals, not a date object with a rain property.
 console.log(goodDaysGoneBad)
+
+// Adding a new line to this file for Lesson 3 homework, made a new branch first.
